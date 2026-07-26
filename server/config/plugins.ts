b1e91abab029e39fcd64a8c37d58a698d7b80142
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-07-26 19:21:21 +0530

    changes in the code

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -9,7 +9,7 @@ const allowedMediaTypes = [
   'application/vnd.openxmlformats-officedocument.*',
   'text/plain',
   'text/csv',
-  'My name is lokesh jaisw'
+  'My name is lokesh jai'
 ];
 
 const deniedExecutableTypes = [
