e93161ad61ed52847399697fb7180a8f837e788b
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-07-27 16:11:56 +0530

    push

--- server/types/generated/components.d.ts
+++ server/types/generated/components.d.ts
@@ -16,6 +16,9 @@ export interface BlocksHeroSection extends Struct.ComponentSchema {
 
 
 
+
+
+
 export interface BlocksInfoBlock extends Struct.ComponentSchema {
   collectionName: 'components_blocks_info_blocks';
   info: {
